49887f10c261131883b08aa7afab53cc1418bd8d
Olga Chavez <olgakatrin827@gmail.com>
2016-10-25 20:16:54 +0000

    saving-my-api-info-for-habitica

--- src/app/config.js
+++ src/app/config.js
@@ -1,2 +1,2 @@
-export const uuId = 'ae71ae62-289b-4e6c-9b6c-c55a10cb641b';
-export const apiToken = '48e78186-1a03-431c-856e-d2c58f38e012';
+export const uuId = 'e6922c82-1068-412a-a6d0-47d6b124f60b';
+export const apiToken = 'd5c6bd53-3173-4336-a5cc-bf63ebdbd3af';
